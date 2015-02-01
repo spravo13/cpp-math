453760c866870894dc3b4818c527b8a46e37dccb
spravo13 <spencer@techandcode.com>
2015-02-01 11:43:03 -0800

    Improve program usability(quitting, continuity)
    Now able to do multiple calculations without having to restart the application. "q" will quit out of the program and if it doesn't recognize the program name it will give directions to see a list of programs. Next step is creating the list in the pprograms function.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,29 @@
 #include <cmath>
 void degree_to_radian();
 void radian_to_degree();
+void programs() {
+
+
+}
+
+
 int main() {
-    using namespace std;
-    string program;
-    cout << "Select a program: ";
-    cin >> program;
-    if(program == "d2r"){
-        degree_to_radian();
-    }
-    else if(program == "r2d"){
-        radian_to_degree();
-    }
+	using namespace std;
+	string program;	
+	while(program != "q"){
+		cout << "Select a program: ";
+		cin >> program;
+		if(program == "q"){
+			cout << "quitting...\n";
+		}
+		else if(program == "d2r"){
+			degree_to_radian();
+		}
+		else if(program == "r2d"){
+			radian_to_degree();
+		}
+		else{
+			cout << "That's not a program, type \"p\" for a list\n";
+		}
+	}
 }
