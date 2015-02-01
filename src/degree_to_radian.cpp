5c4d3e34c442aeb26af46b23c50f34604e890785
spravo13 <spencer@techandcode.com>
2015-01-31 17:23:16 -0800

    Add degree/radian conversions to programs.

---
+++ src/degree_to_radian.cpp
@@ -0,0 +1,10 @@
+void degree_to_radian() {
+    const double pi = 4.0*atan(1.0);
+    using namespace std;
+    float d, r, rpi;
+    cout << "Enter degrees: ";
+    cin >> d;
+    r = d*(pi/180);
+    rpi = d/180;
+    cout << r << " or " << rpi << "π radians\n";
+}
---
+++ src/radian_to_degree.cpp
@@ -0,0 +1,9 @@
+void radian_to_degree() {
+    const double pi = 4.0*atan(1.0);
+    using namespace std;
+    float r, d;
+    cout << "Enter the number of radians: ";
+    cin >> r;
+    d = r*(180/pi);
+    cout << d << " degrees \n";
+}
