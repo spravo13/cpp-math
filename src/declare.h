bc84e9963beac465aa68a18e6372fdc7cc07fd83
spravo13 <spencer@techandcode.com>
2015-02-08 22:22:49 -0800

    Add file to declare functions in rather than main

---
+++ src/declare.h
@@ -0,0 +1,8 @@
+void degree_to_radian();
+void radian_to_degree();
+void circle_area();
+void trapezoid_area();
+void parallelogram_area();
+void triangle_area();
+void cylinder_surface_area();
+void cylinder_volume();
