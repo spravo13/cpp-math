e2ead069ce365791e1076e91d7d1b31348109314
spravo13 <spencer@techandcode.com>
2015-02-09 17:55:53 -0800

    Quadform; Add discriminant check.
    Checks to see how many solutions there are so it only shows real solutions.

--- src/quadratic_formula.cpp
+++ src/quadratic_formula.cpp
@@ -7,6 +7,7 @@ void quadratic_formula() {
 
 	const double pi = 4.0*atan(1.0);
 	float a, b, c, d, qf1, qf2;
+	int dis;
 
 	cout << "A value: ";
 	cin >> a;
@@ -15,12 +16,21 @@ void quadratic_formula() {
 	cout << "C value: ";
 	cin >> c;
 
-	d = (pow(b,2)) - (4*a*c);
-	d = pow(d,.5);
-	qf1 = (-b + d) / 2 * a;
-	qf1 = (-b - d) / 2 * a;
+	dis = pow(b,2) - (4 * a * c);
+	
+	if (dis < 0) {
+		cout << "No real solutions\n";
+	} else {	
+		d = (pow(b,2)) - (4*a*c);
+		d = pow(d,.5);
+		qf1 = (-b + d) / 2 * a;
+		qf1 = (-b - d) / 2 * a;
 
-	cout << "Solution 1: (" << qf1 << ",0)\n";
-	cout << "Solution 2: (" << qf2 << ",0)\n";
-
+		if(dis == 0) {
+			cout << "Solution: (" << qf1 << ",0)\n";
+		} else {
+			cout << "Solution 1: (" << qf1 << ",0)\n";
+			cout << "Solution 2: (" << qf2 << ",0)\n";
+		}
+	}
 }
