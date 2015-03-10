09800e56ba6e19dc81318eeb1a41802f38f8fdf8
spravo13 <spencer@techandcode.com>
2015-03-09 23:13:56 -0700

    csa; change pi to M_PI

--- src/cylinder_surface_area.cpp
+++ src/cylinder_surface_area.cpp
@@ -12,7 +12,7 @@ void cylinder_surface_area() {
 	cout << "Height of cylinder: ";
 	cin >> h;
 
-	sa = ((2*M_PI*r) * h) + ((pi*pow(r,2)*2));
+	sa = ((2*M_PI*r) * h) + ((M_PI*pow(r,2)*2));
 
 	cout << "Surface area is: " << sa << "\n";
 
