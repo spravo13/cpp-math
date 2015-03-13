74a7ba54be16339f970d6ba02917e08164543305
spravo13 <spencer@techandcode.com>
2015-03-12 22:00:58 -0700

    qf; fix equation. Wrong variables.

--- src/quadratic_formula.cpp
+++ src/quadratic_formula.cpp
@@ -22,8 +22,8 @@ void quadratic_formula() {
 	} else {	
 		d = (pow(b,2)) - (4*a*c);
 		d = pow(d,.5);
-		qf1 = (-b + d) / 2 * a;
-		qf1 = (-b - d) / 2 * a;
+		qf1 = (-b + d) / (2 * a);
+		qf2 = (-b - d) / (2 * a);
 
 		if(dis == 0) {
 			cout << "Solution: (" << qf1 << ",0)\n";
